2274e5bb4188a5a7a0e06323f395a3e518369f3e
DSHOUSE\Nguyen <nguyenduy.math@outlook.com>
2024-01-21 10:50:44 +0700

    clear code

--- ilearnapprunner/src/main/java/com/bigdata/apprunner/config/AwsSqsConfig.java
+++ ilearnapprunner/src/main/java/com/bigdata/apprunner/config/AwsSqsConfig.java
@@ -5,20 +5,12 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.converter.MessageConverter;
-import org.springframework.messaging.handler.annotation.support.PayloadMethodArgumentResolver;
 
 import com.amazonaws.services.sqs.AmazonSQSAsync;
-import com.amazonaws.services.sqs.buffered.AmazonSQSBufferedAsyncClient;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import io.awspring.cloud.messaging.config.QueueMessageHandlerFactory;
-import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
-
-import java.util.Collections;
 
 @Configuration
 public class AwsSqsConfig {
