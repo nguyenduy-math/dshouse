3f190b34dd64e2600bdb5ee2605dfbdfb100aa82
nguyenduy-android <nguyenduy.android@gmail.com>
2024-01-21 12:54:53 +0700

    clear code

--- ilearnapprunner/src/main/java/com/bigdata/apprunner/config/AwsSqsConfig.java
+++ ilearnapprunner/src/main/java/com/bigdata/apprunner/config/AwsSqsConfig.java
@@ -1,43 +1,43 @@
 package com.bigdata.apprunner.config;
 
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import com.amazonaws.auth.AWSStaticCredentialsProvider;
+// import com.amazonaws.auth.BasicAWSCredentials;
+// import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import com.amazonaws.services.sqs.AmazonSQSAsync;
+// import com.amazonaws.services.sqs.AmazonSQSAsync;
 
 
-@Configuration
-public class AwsSqsConfig {
+// @Configuration
+// public class AwsSqsConfig {
 
-    @Value("${aws.sqs.accessKeyId}")
-    private String accessKey;
+//     @Value("${aws.sqs.accessKeyId}")
+//     private String accessKey;
 
-    @Value("${aws.sqs.secretKey}")
-    private String secretKey;
+//     @Value("${aws.sqs.secretKey}")
+//     private String secretKey;
 
-    @Value("${aws.sqs.region}")
-    private String region;
+//     @Value("${aws.sqs.region}")
+//     private String region;
 
-    @Value("${aws.sqs.queueName}")
-    private String queueName;
+//     @Value("${aws.sqs.queueName}")
+//     private String queueName;
 
-    @Bean
-    public AmazonSQSAsync amazonSQSAsync() {
-        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-        AmazonSQSAsyncClientBuilder builder = AmazonSQSAsyncClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region);
-        return builder.build();
-    }
+//     @Bean
+//     public AmazonSQSAsync amazonSQSAsync() {
+//         BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+//         AmazonSQSAsyncClientBuilder builder = AmazonSQSAsyncClientBuilder.standard()
+//                 .withCredentials(new AWSStaticCredentialsProvider(credentials))
+//                 .withRegion(region);
+//         return builder.build();
+//     }
 
-    @Bean
-    public QueueMessagingTemplate queueMessagingTemplate(AmazonSQSAsync amazonSQSAsync) {
-        return new QueueMessagingTemplate(amazonSQSAsync);
-    }
-}
+//     @Bean
+//     public QueueMessagingTemplate queueMessagingTemplate(AmazonSQSAsync amazonSQSAsync) {
+//         return new QueueMessagingTemplate(amazonSQSAsync);
+//     }
+// }
