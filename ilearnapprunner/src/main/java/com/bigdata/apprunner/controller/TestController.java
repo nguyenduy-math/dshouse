d47c4bdc588eaac194c7f038f6736f28d2edc6cc
nguyenduy-android <nguyenduy.android@gmail.com>
2024-01-30 23:11:57 +0700

    clear code

--- ilearnapprunner/src/main/java/com/bigdata/apprunner/controller/TestController.java
+++ ilearnapprunner/src/main/java/com/bigdata/apprunner/controller/TestController.java
@@ -1,8 +1,8 @@
 package com.bigdata.apprunner.controller;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +12,10 @@ import org.bson.Document;
 @RestController
 @RequestMapping(value="/test-apis", produces = MediaType.APPLICATION_JSON_VALUE)
 public class TestController {
-    // @Value("${aws.sqs.queueName}")
-    // private String queueName;
-    // @Autowired
-    // private QueueMessagingTemplate queueMessagingTemplate;
+    @Value("${aws.sqs.queueName}")
+    private String queueName;
+    @Autowired
+    private QueueMessagingTemplate queueMessagingTemplate;
 
     @GetMapping("/talk/{text}")
     public ResponseEntity<Document> test(
@@ -34,7 +34,7 @@ public class TestController {
         var doc = new Document();
         doc.put("message","Hello World," + text);
 
-        // queueMessagingTemplate.convertAndSend(queueName, doc.get("message"));
+        queueMessagingTemplate.convertAndSend(queueName, doc.get("message"));
 
         return new ResponseEntity<Document>(doc, HttpStatus.OK);
     }
