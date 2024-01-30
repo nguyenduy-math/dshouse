42b3a181b1a1aba200e6d63d660f8f8e7cc5c422
DSHOUSE\Nguyen <nguyenduy.math@outlook.com>
2024-01-31 00:36:51 +0700

    init the project

---
+++ ilearnspringboot/src/main/java/com/bigdata/newmission/IlearnSpringboot.java
@@ -0,0 +1,13 @@
+package com.bigdata.newmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlearnSpringboot {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlearnSpringboot.class, args);
+	}
+
+}
---
+++ ilearnspringboot/src/test/java/com/bigdata/newmission/IlearnSpringbootTests.java
@@ -0,0 +1,13 @@
+package com.bigdata.newmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlearnSpringbootTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
